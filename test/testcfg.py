d7261194c7cf041f161c43db7f3dfe0d13d89975
Rod Conan <rconan@gmail.com>
2015-06-25 15:47:04 -0700

    ADDED BATCH TEST SCRIPT

---
+++ test/testcfg.py
@@ -0,0 +1,6 @@
+c = get_config()
+c.NbConvertApp.notebooks = ["../atmosphere/Atmosphere.ipynb",
+                            "../atmosphere/AtmospherePhaseScreenGradient.ipynb",
+                            "../LMMSE/LaserTomography.ipynb",
+                            "../shackHartmann/Shack-HartmannWavefrontSensor.ipynb",
+                            "../shackHartmann/Shack-HartmannNoisePropagation.ipynb"]
