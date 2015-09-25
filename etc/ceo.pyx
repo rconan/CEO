df52f11e647aaa5c559bb582b316e63f0d868861
Rod Conan <rconan@gmto.org>
2015-09-25 20:24:25 +0000

    NEW PYTHON DOC GIT CLONE

--- etc/ceo.pyx
+++
@@ -1,8 +0,0 @@
-"ceo.pyx"
-
-import math
-import numpy as np
-cimport cython
-cimport numpy as np
-cimport ceo
-
--- test/testcfg.py
+++
@@ -1,6 +0,0 @@
-c = get_config()
-c.NbConvertApp.notebooks = ["../atmosphere/Atmosphere.ipynb",
-                            "../atmosphere/AtmospherePhaseScreenGradient.ipynb",
-                            "../LMMSE/LaserTomography.ipynb",
-                            "../shackHartmann/Shack-HartmannWavefrontSensor.ipynb",
-                            "../shackHartmann/Shack-HartmannNoisePropagation.ipynb"]
