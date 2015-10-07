18107532bc1b1878885f9dee1b1e32412f4df930
Rod Conan <rconan@gmto.org>
2015-10-07 04:42:57 +0000

    IMAGING AND CENTROIDING CLASS AS POINTER IN SHACKHARTMANN

--- python/ceo/__init__.py
+++ python/ceo/__init__.py
@@ -2,8 +2,8 @@ import constants
 from utilities import cuFloatArray, cuDoubleArray, cuIntArray, MaskAbstract, Mask, Telescope, GMT, StopWatch
 from source import Complex_amplitude, Source
 from atmosphere import Atmosphere, GmtAtmosphere
-from centroiding import _Centroiding_, Centroiding
-from imaging import _Imaging_, Imaging
+from centroiding import Centroiding
+from imaging import Imaging
 from shackHartmann import ShackHartmann
 from LMMSE import Lmmse, LmmseSH
 from rayTracing import Bundle, ZernikeS, GMT_M1, GMT_M2, Coordinates
