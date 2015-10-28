56d09d7e5795297b95587ff3f2f25708e0cc5fd6
Rod Conan <rconan@gmail.com>
2015-10-28 16:46:49 -0700

    ADDED A NEW TEST

--- test/testcfg.py
+++ test/testcfg.py
@@ -1,7 +1,9 @@
 c = get_config()
+c.ExecutePreprocessor.timeout = 300
 c.NbConvertApp.notebooks = ["../atmosphere/Atmosphere.ipynb",
                             "../atmosphere/AtmospherePhaseScreenGradient.ipynb",
                             "../LMMSE/LaserTomography.ipynb",
                             "../shackHartmann/Shack-HartmannWavefrontSensor.ipynb",
                             "../shackHartmann/Shack-HartmannNoisePropagation.ipynb",
+                            "../shackHartmann/LtaoShWfs.ipynb",
                             "../rayTracing/GMT-Ray-Tracing.ipynb"]
