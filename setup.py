05e7228948dacaf9755ec97ce636188978a97147
Rod Conan <rconan@gmto.org>
2016-02-22 17:01:45 +0000

    ADDED DURATION PARMETER TO ATMOSPHERE CLASS

---
+++ setup.py
@@ -0,0 +1,12 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='ceo',
+      version='1.0',
+      description='Cuda--Engined Optics',
+      author='Rodolphe Conan',
+      author_email='conan.rod@gmail.com',
+      url='http://rconan.github.io/CEO/',
+      packages=['python.ceo'],
+     )
