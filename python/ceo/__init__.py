48cdd21e9f2ed7f256a18dbbe9eaf3ab6e7b42e9
Rod Conan <rconan@gmto.org>
2016-03-07 17:19:20 +0000

    JSON CLASSES, FLOOR IMAGING FRAME

--- python/ceo/__init__.py
+++ python/ceo/__init__.py
@@ -1,15 +1,15 @@
 import constants
 from utilities import cuFloatArray, cuDoubleArray, cuIntArray, MaskAbstract, Mask, Telescope, GMT, StopWatch, SparseMatrix, SparseGradient, wavefrontFiniteDifference
-from source import Bundle, Complex_amplitude, Source
+from source import Bundle, Complex_amplitude, Source, JSource
 from rayTracing import ZernikeS, Coordinates, Quaternion
 from atmosphere import AtmosphereAbstract, Atmosphere, GmtAtmosphere, Layer
 from centroiding import Centroiding
-from imaging import Imaging
+from imaging import Imaging, JImaging
 from shackHartmann import ShackHartmann
 from pyramid import Pyramid
 from segmentPistonSensor import SegmentPistonSensor
 from aaStats import AaStats, PaStats
 from LMMSE import Lmmse, LmmseSH, BilinearInterpolation
 from gmtMirrors import GmtMirrors, GMT_M1, GMT_M2, StereoscopicEdgeSensors, LateralEdgeSensors, DistanceEdgeSensors
-from GMTLIB import GMT_MX, TT7, IdealSegmentPistonSensor, SegmentTipTiltSensor, EdgeSensors, DispersedFringeSensor
+from GMTLIB import GMT_MX, JGMT_MX, TT7, IdealSegmentPistonSensor, SegmentTipTiltSensor, EdgeSensors, DispersedFringeSensor
 import phaseStats
