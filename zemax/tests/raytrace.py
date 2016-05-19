fa2a39b812c89204991f9671046364853a42978f
Rod Conan <rconan@gmto.org>
2016-05-19 19:07:11 +0000

    SOME WORKS ON TILTED SURFACES

--- zemax/tests/raytrace.py
+++ zemax/tests/raytrace.py
@@ -1,10 +1,11 @@
 import ceo
 import numpy as np
+import sys
 
 def chief(src):
-    print 'XYZ: '+np.array_str(src.rays.chief_coordinates.host())
-    print 'KLM: '+np.array_str(src.rays.chief_directions.host())
-    
+    print '  . CHIEF XYZ: '+np.array_str(src.rays.chief_coordinates.host(),precision=4)
+    print '  . CHIEF KLM: '+np.array_str(src.rays.chief_directions.host(),precision=4)
+     
 def p_ray(src, ray_idx):
     print 'XYZ: '+np.array_str(src.rays.coordinates.host()[ray_idx])
     print 'KLM: '+np.array_str(src.rays.directions.host()[ray_idx])
@@ -13,37 +14,52 @@ def raytrace(src,S,idx,xyz):
     _S_ = S[idx-1]
     ceo.Transform_to_S(src,_S_)
 
-    if not _S_.coord_break: 
+    if _S_.coord_break: 
+        msg = '#%d: coordinate break'%idx
+    else:
         ceo.Intersect(src,_S_)
         n_S = _S_.refractive_index(src)
-        print 'Material refractive index: %.9f'%n_S
+        msg = '#%d: refractive index: %.3f'%(idx,n_S)
         if n_S==-1:
-            print "reflecting"
+            msg += ", reflection"
             ceo.Reflect(src)
         else:
             mu = src.rays.refractive_index/n_S
             if mu!=1.0:
+                msg += ", refraction"
                 ceo.Refract(src,mu)
             src.rays.refractive_index = n_S
         if _S_.tilted_surface:
-            print "TILTED"
+            msg += ", tilted"
+            #chief(src)
+            #ceo.Transform_to_R(src,_S_)
+            #_S_.ref_frame.euler_angles[:] = np.zeros(3)
+            #ceo.Transform_to_S(src,_S_)
+            chief(src)
+            _S__origin = _S_.ref_frame.origin[:]
+            _S_.ref_frame.origin[:] = np.zeros(3)
             ceo.Transform_to_R(src,_S_)
+            _S_.ref_frame.origin[:] =  _S__origin
+            chief(src)
+
+        #p_ray(src, 17)
     
         #print 'To GCS:'
         for k in range(idx-1,-1,-1):
             #print k
-            if not S[k].tilted_surface:
-                ceo.Transform_to_R(src,S[k])
+            #if not S[k].tilted_surface:
+            ceo.Transform_to_R(src,S[k])
         xyz.append(src.rays.coordinates.host())
-
-        p_ray(src, 17)
-
+        
         if idx<len(S):
             #print 'To last surface CS:'
             for k in range(idx):
                 #print k
                 ceo.Transform_to_S(src,S[k])
-
+               
+    sys.stdout.write(msg+"\n")
+    chief(src)
+ 
 def coords(xyz, ray_idx, xyz_idx):
     return [w[ray_idx][xyz_idx] for w in xyz]
 
