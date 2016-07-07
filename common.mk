a5582b7ea40697547aa95896f0e7b77eda1ce114
Rod Conan <rconan@gmto.org>
2016-07-07 21:26:32 +0000

    MOVE TRACE FROM RAYTRACING TO GMTLIB

--- common.mk
+++ common.mk
@@ -27,6 +27,8 @@ sobj    = $(nwsrc:%.nw=%.so)
 cusrc  = $(nwsrc:%.nw=%.cu)
 nwcysrc  = $(nwsrc:%.nw=%.cython.nw)
 texcysrc = $(nwcysrc:%.nw=%.tex)
+pxdcysrc  = $(nwcysrc:%.nw=%.pxd)
+pyxcysrc  = $(nwcysrc:%.nw=%.pyx)
 pxdsrc  = $(nwcysrc:%.cython.nw=%.pxd)
 pyxsrc  = $(nwcysrc:%.cython.nw=%.pyx)
 libsrc = $(CEOPATH)/lib/libceo.a 
@@ -68,10 +70,10 @@ libsrc = $(CEOPATH)/lib/libceo.a
 	$(TANGLE) -R$@ $< > $@
 
 .nw.pxd:
-	$(TANGLE) -R$@ $(nwcysrc) > $@
-	cp $@ $(CEOPYPATH)/
+	$(TANGLE) -R$(pxdsrc) $< > $@
+	cp $@ $(CEOPYPATH)/$(pxdsrc)
 
 .nw.pyx:
-	$(TANGLE) -R$@  $(nwcysrc) > $@
-	cp $@ $(CEOPYPATH)/
+	$(TANGLE) -R$(pyxsrc)  $< > $@
+	cp $@ $(CEOPYPATH)/$(pyxsrc)
 
