af949fa4c3e4b1155430067555fa98c20214c9b5
Rod Conan <rconan@gmto.org>
2016-07-22 14:27:30 +0000

    BENDING MODES CYTHON INTERFACE

--- python/ceo/GMTLIB.py
+++ python/ceo/GMTLIB.py
@@ -71,12 +71,14 @@ class GMT_MX(GmtMirrors):
     >>> gpu_ps1d = src.wavefront.phase()
     """
     def __init__(self, D=None, D_px=None, M1_radial_order=0, M2_radial_order=0,
-                 M1_mirror_modes="zernike", M2_mirror_modes="zernike"):
+                 M1_mirror_modes="zernike", M2_mirror_modes="zernike",
+                 M1_N_MODE=0):
         super(GMT_MX,self).__init__(
                             M1_radial_order=M1_radial_order,
                             M2_radial_order=M2_radial_order,
                             M1_mirror_modes=M1_mirror_modes,
-                            M2_mirror_modes=M2_mirror_modes)
+                            M2_mirror_modes=M2_mirror_modes,
+                            M1_N_MODE=M1_N_MODE)
 
     def calibrate(self,wfs,gs,mirror=None,mode=None,stroke=None,segment=None,agws=None,recmat=None,first_mode=3,minus_M2_TT=False):
         """
