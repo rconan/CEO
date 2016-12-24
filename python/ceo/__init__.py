9cd1b062cb2a4f322387041637e632064dd7e535
Fernando Quiros-Pacheco <fquiros@gmto.org>
2016-12-24 00:07:37 +0000

    Imaging: Noiseless frame readOut implemented.

--- python/ceo/__init__.py
+++ python/ceo/__init__.py
@@ -2,15 +2,15 @@ import constants
 from utilities import cuFloatArray, cuDoubleArray, cuIntArray, MaskAbstract, Mask, Telescope, GMT, StopWatch, SparseMatrix, SparseGradient, wavefrontFiniteDifference
 from source import Bundle, Complex_amplitude, Source, JSource
 from rayTracing import ZernikeS, Coordinates, Coordinate_system, Quaternion, Aperture, Conic, Transform_to_S, Transform_to_R, Intersect, Reflect, Refract
-from atmosphere import AtmosphereAbstract, Atmosphere, GmtAtmosphere, Layer, JGmtAtmosphere
-from centroiding import Centroiding
 from imaging import Imaging, JImaging
+from centroiding import Centroiding
 from shackHartmann import ShackHartmann, GeometricShackHartmann, JShackHartmann
 from pyramid import Pyramid
 from segmentPistonSensor import SegmentPistonSensor
 from aaStats import AaStats, PaStats
 from LMMSE import Lmmse, LmmseSH, BilinearInterpolation
 from gmtMirrors import BendingModes, GmtMirrors, GMT_M1, GMT_M2, StereoscopicEdgeSensors, LateralEdgeSensors, DistanceEdgeSensors
+from atmosphere import AtmosphereAbstract, Atmosphere, GmtAtmosphere, Layer, JGmtAtmosphere
 from GMTLIB import GMT_MX, JGMT_MX, TT7, IdealSegmentPistonSensor, SegmentTipTiltSensor, EdgeSensors, DispersedFringeSensor, Trace
 import phaseStats
                                 
