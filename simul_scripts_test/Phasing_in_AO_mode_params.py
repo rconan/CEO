e257a5253fdd857e2175168da7daa89d4c157465
Fernando Quiros <fquiros@gmto.org>
2017-04-05 17:12:14 +0000

    SIMUL: Notebook for on-axis DFS sensitivity analysis with dark current

--- simul_scripts_test/Phasing_in_AO_mode_params.py
+++ simul_scripts_test/Phasing_in_AO_mode_params.py
@@ -10,15 +10,15 @@ Tsim = 2e-3        # Simulation time step [seconds]
 totSimulTime = 0.5 # Total simulated time [seconds]
 
 #----- System configurations:
-simul_turb         = True
-simul_onaxis_AO    = True
+simul_turb         = False
+simul_onaxis_AO    = False
 simul_PS_control   = True
-simul_FDSP_control = True  #Note: If FDSP in ON, on-axis AO needs to be ON too
+simul_FDSP_control = False  #Note: If FDSP in ON, on-axis AO needs to be ON too
 
 eval_perf_onaxis    = True
 eval_perf_field     = False
 eval_perf_modal     = False   # project Residual phase
-eval_perf_sps       = True
+eval_perf_sps       = False
 
 if simul_onaxis_AO==True or simul_FDSP_control==True:
     simul_SH = True
@@ -36,34 +36,36 @@ if simul_turb == True:
 #----- SPS guide stars and sensors:
 if simul_SPS==True:
     SPStype = 'DFS'         # Choose between "ideal" or "DFS"
-    asterism_type = 'Besancon_GP_AB' #Choose between 'Besancon_GP' and 'Dummy'
-    N_GS_PS = 3
-    alpha_ps = 6.0*60.      # radius of circle where GSs are located [in arcsec]
+    asterism_type = 'Dummy' #Choose between 'Besancon_GP', 'Besancon_GP_AB', and 'Dummy'
+    N_GS_PS = 1
+    alpha_ps = 0. #6.0*60.      # radius of circle where GSs are located [in arcsec]
     band = "J"
     mag = 12.0
     bkgd_mag = 16.2         # J-band sky bkgd (mag/arcsec^2); Tech Note GMT-02274, rev 2.4
-    e0 = 1.88e12             # ph/s in J band
+    e0 = 1.88e12/368.*PupilArea  # ph/s in J band over the GMT pupil
     throughput = 0.65*0.75   # Table 3, GMT-DOC-01404
     sps_fov = 2.8           # arcsec diameter
     sps_dispersion = 5.0
-    RONval = 0.4            # [e- rms]
+    RONval = 0.35            # [e- rms]
     sps_mask_size = 1.5     # arcsec
     lobe_detection = 'peak_value'
     nyquist_factor = 1
-
+    dark_cur = 48.0 # additional background in e-/pix/s (dark current + allocated cryo filter leak) 
+    excess_noise = 1.25
+ 
     simul_phot = True
     simul_bkgd = True
     sps_seed = 1928
     
-    gPS   = 0.0
+    gPS   = 0.
     gFDSP = 0.6
 
-    exposureTime = 30e-3 # DFS camera integration time [seconds]
+    exposureTime = 10e-3 # DFS camera integration time [seconds]
     samplingTime = 30.0  # DFS sampling time [seconds]
     sps_sampl_delay = 3  # number of exposures to neglect after FDSP correction
     # (to avoid M1-M2 transient)
-    sps_sampl_iter = 9
-    totSimulTime = sps_sampl_iter*samplingTime + exposureTime
+    sps_sampl_iter = 10
+    totSimulTime = sps_sampl_iter*samplingTime #+ exposureTime
 
 #---- ON-AXIS AO system parameters:
 if simul_SH == True:
