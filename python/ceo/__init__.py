0df2a1548ab879be9d349a8d40c3b42c5669e474
Rod Conan <rconan@gmto.org>
2017-09-15 18:48:18 +0000

    MODES modes in C++ and Modes in Cython generalized the bendingModes/Bending modes structure and class

--- python/ceo/__init__.py
+++ python/ceo/__init__.py
@@ -10,7 +10,7 @@ from segmentPistonSensor import SegmentPistonSensor
 from aaStats import AaStats, PaStats
 from LMMSE import Lmmse, LmmseSH, BilinearInterpolation
 from atmosphere import AtmosphereAbstract, Atmosphere, GmtAtmosphere, Layer, JGmtAtmosphere
-from gmtMirrors import BendingModes, KarhunenLoeve, GmtMirrors, GMT_M1, GMT_M2, StereoscopicEdgeSensors, LateralEdgeSensors, DistanceEdgeSensors
+from gmtMirrors import BendingModes, KarhunenLoeve, GmtMirrors, GMT_M1, GMT_M2, StereoscopicEdgeSensors, LateralEdgeSensors, DistanceEdgeSensors, Modes
 from GMTLIB import CalibrationVault, GMT_MX, JGMT_MX, GeometricTT7, IdealSegmentPistonSensor, SegmentTipTiltSensor, EdgeSensors, DispersedFringeSensor, Trace
 import phaseStats
                                 
