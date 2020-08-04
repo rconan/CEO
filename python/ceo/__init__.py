1a4c0010390d74d86476370bda9f886bb1cac66b
Rod Conan <rconan@gmto.org>
2020-08-04 08:14:20 -0700

    MAJOR FIX OF ATMOSPHERE TOMOGRAPHY ALGORITHM

--- python/ceo/__init__.py
+++ python/ceo/__init__.py
@@ -7,7 +7,10 @@ from .imaging import Imaging, JImaging
 from .centroiding import Centroiding
 from .shackHartmann import ShackHartmann, TT7, GeometricShackHartmann, JShackHartmann
 from .segmentPistonSensor import SegmentPistonSensor
-from .aaStats import AaStats, PaStats
+from .aaStats import AaStats, AaStatsMatrix, PaStats
+from .BTBT import Btbt
+from .GBTBT import Gbtbt
+from .iterativeSolvers import Minres
 from .LMMSE import Lmmse, LmmseSH, BilinearInterpolation
 from .atmosphere import AtmosphereAbstract, Atmosphere, GmtAtmosphere, Layer, JGmtAtmosphere
 from .gmtMirrors import BendingModes, KarhunenLoeve, GmtMirrors, GMT_M1, GMT_M2, StereoscopicEdgeSensors, LateralEdgeSensors, DistanceEdgeSensors, Modes
