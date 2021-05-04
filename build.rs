8b452ccf4890d1147a195fbb96db1b2bcb555f95
Rod Conan <rconan@gmto.org>
2021-05-04 11:46:29 -0700

    BUILD SCRIPT
    Add `make clear` and rerun option

--- build.rs
+++ build.rs
@@ -1,7 +1,9 @@
 use std::process::Command;
 
 fn main() {
+    println!("cargo:rerun-if-changed=build.rs");
     let make_ceo = Command::new("make")
+        .arg("clean")
         .arg("all")
         .output()
         .expect("failed to make ceo");
