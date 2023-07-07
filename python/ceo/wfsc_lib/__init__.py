7036fd6e63ef7aebfe20d64444ea8c7fd4da342e
Fernando Quiros-Pacheco <fquiros@gmto.org>
2023-07-07 17:52:49 +0000

    WFSC_LIB: MirrorSimulator class

--- python/ceo/wfsc_lib/__init__.py
+++ python/ceo/wfsc_lib/__init__.py
@@ -1,4 +1,5 @@
 from .IIRfilter import IIRfilter
 from .MirrorPositioner import MirrorPositioner
+from .MirrorSimulator import MirrorSimulator
 from .gmt_pupil_model import gmt_pupil_model
 from .poly_winding_number import poly_winding_number
