4d6273a8f3cd5f0d0bbe14bb6f4336da643f9377
Fernando Quiros-Pacheco <fquiros@gmto.org>
2023-07-11 07:08:35 +0000

    WFSC_LIB: removed MirrorPositioner class (replaced by MirrorSimulator)

--- python/ceo/wfsc_lib/__init__.py
+++ python/ceo/wfsc_lib/__init__.py
@@ -1,5 +1,4 @@
 from .IIRfilter import IIRfilter
-from .MirrorPositioner import MirrorPositioner
 from .MirrorSimulator import MirrorSimulator
 from .gmt_pupil_model import gmt_pupil_model
 from .poly_winding_number import poly_winding_number
